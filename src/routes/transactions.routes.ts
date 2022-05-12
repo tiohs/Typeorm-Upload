c2f67096c08dd0a17a6223aec2cc82cb3483154e
tiohs <hamiltonsilva551@gmail.com>
2022-05-12 15:42:55 +0100

    updated

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,37 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.get('/', async (request, response) => {
+//   // TODO
+// });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  console.log(title)
+  const createTransection = new CreateTransactionService();
+
+  const transaction = await createTransection.execute({
+    title,
+    value,
+    type,
+    category
+  });
+  return response.json(transaction);
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.delete('/:id', async (request, response) => {
+//   TODO
+// });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.post('/import', async (request, response) => {
+//   TODO
+// });
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,23 @@
 // import AppError from '../errors/AppError';
-
+import { response } from 'express';
+import { getCustomRepository  } from 'typeorm';
 import Transaction from '../models/Transaction';
-
+import TransactionRepository from '../repositories/TransactionsRepository';
+interface IRequest {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category} : IRequest ): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const transaction = transactionsRepository.create({
+        title, value, type
+    })
+
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
