eb998e49cfe68d395d3fe38f998ee838eeac807a
tiohs <hamiltonsilva551@gmail.com>
2022-05-12 15:50:39 +0100

    responde transection

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-// transactionsRouter.get('/', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.get('/', async (request, response) => {
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+  const transaction = await transactionRepository.find();
+  
+  return response.json(transaction);
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
