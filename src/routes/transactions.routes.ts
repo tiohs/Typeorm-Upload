ae9f398f47f120bef97d68701851234c0b7e5f90
tiohs <hamiltonsilva551@gmail.com>
2022-05-12 20:26:36 +0100

    ok

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -30,9 +30,14 @@ transactionsRouter.post('/', async (request, response) => {
   return response.json(transaction);
 });
 
-// transactionsRouter.delete('/:id', async (request, response) => {
-//   TODO
-// });
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
+  return response.status(204).send();
+});
 
 // transactionsRouter.post('/import', async (request, response) => {
 //   TODO
