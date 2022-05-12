98dd4278390df0b60e9457a9b8adbcf358024c1e
tiohs <hamiltonsilva551@gmail.com>
2022-05-12 15:17:18 +0100

    updated

--- src/database/migrations/1651605555815-CreateTransactions.ts
+++ src/database/migrations/1651605555815-CreateTransactions.ts
@@ -34,7 +34,7 @@ export default class CreateTransactions1651605555815
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
